db5fcb9cc032f8924c0572dcbf8b004d796a0e84
Tyler Reiser <reiser.tyler@gmail.com>
2022-01-28 19:11:34 -0700

    updated parameters

--- Lorenz_RK4.py
+++ Lorenz_RK4.py
@@ -84,13 +84,10 @@ for i in range(n - 1):  # loop the stages
 fig = plt.figure('Lorenz System of ODEs', figsize=(12, 8), dpi=100)
 ax = fig.gca(projection='3d')
 
-print(term[:i, 0])
-print(term[:i, 1])
-print(term[:i, 2])
 
 
 def animate(i):  # Anim function; original version set default grid to off
-    ax.view_init(20, -10 + i/2)
+    ax.view_init(20, i/3) # sets rotation by i/3 for each step
     ax.clear()
     ax.set(facecolor='white')
     ax.plot(term[:i, 0], term[:i, 1], term[:i, 2],
@@ -101,5 +98,5 @@ def animate(i):  # Anim function; original version set default grid to off
 
 
 ani = animation.FuncAnimation(
-    fig, animate, np.arange(100), interval=5, repeat=False)  # anim object; 2k frame, 5ms delay
+    fig, animate, np.arange(2000), interval=5, repeat=False)  # anim object; 2k frame, 5ms delay
 plt.show()
